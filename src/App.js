c907b5e63545959ca0d2cad29d143f120e00605d
KobeanH <90979360+KobeanH@users.noreply.github.com>
2021-11-04 23:39:26 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecoundaryButton } from "./components/atoms/button/SecoundaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "じゃけえ",
+  image: "https://kobeans-blog.com/site/img/kob.jpg",
+  email: "aaa@gmail.com",
+  phone: "000-9999-8888",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecoundaryButton>検索</SecoundaryButton>
+      <br />
+      <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
